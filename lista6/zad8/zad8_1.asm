53e537b6a86838b2b2f4797aeee675defaa31510
dzazef <dzazef@gmail.com>
2019-01-06 21:16:43 +0100

    Corrected l6zad8_1.

--- lista6/zad8/zad8_1.asm
+++ lista6/zad8/zad8_1.asm
@@ -15,32 +15,45 @@ main:
   push scan
   call scanf
   
+  ;nie pokazuje niepotrzebnych wartości na stosie
   finit					;moduł zmiennoprzecinkowy
-  fld qword [inp]		  ;st(0) = x
+  fld qword [inp]		;st(0) = x
   fldl2e				;st(1) = x | st(0) = log2(e)
-  fyl2x					;st(0) = x*log2(e)
-  f2xm1					;st(0) = 2^(x*log2(e))-1
-  fld1					;st(1) = 2^(x*log2(e))-1 | st(0) = 1
-  faddp					;st(0) = 2^(x*log2(e))
+  fmulp					;st(0) = x*log2(e)
+  fld st0				;st(1) = x*log2(e) | st(0) = x*log2(e)
+  frndint				;st(1) = x*log2(e) | st(0) = INT[x*log2(e)]
+  fsub st1,st0			;st(1) = x*log2(e)-INT[x*log2(e)] | st(0) = INT[x*log2(e)]
+  fxch st1				;st(1) = INT[x*log2(e)] | st(0) = x*log2(e)-INT[x*log2(e)]
+  f2xm1					;st(1) = INT[x*log2(e)] | st(0) = 2^(x*log2(e)-INT[x*log2(e)])-1
+  fld1					;st(2) = INT[x*log2(e)] | st(1) = 2^(x*log2(e)-INT[x*log2(e)])-1 | st(0) = 1
+  faddp					;st(1) = INT[x*log2(e)] | st(0) = 2^(x*log2(e)-INT[x*log2(e)])
+  fscale				;st(0) = 2^(x*log2(e))
   fld1					;st(1) = 2^(x*log2(e)) | st(0) = 1
   fld1					;st(2) = 2^(x*log2(e)) | st(1) = 1 | st(0) = 1
   faddp					;st(1) = 2^(x*log2(e)) | st(0) = 2
   fdivp					;st(0) = 2^(x*log2(e))/2
-  fld qword [inp]		  ;st(1) = 2^(x*log2(e))/2 | st(0) = x
+  fld qword [inp]		;st(1) = 2^(x*log2(e))/2 | st(0) = x
   fchs					;st(1) = 2^(x*log2(e))/2 | st(0) = -x
   fldl2e				;st(2) = 2^(x*log2(e))/2 | st(1) = -x | st(0) = log2(e)
-  fyl2x					;st(1) = 2^(x*log2(e))/2 | st(0) = -x*log2(e)
-  f2xm1					;st(1) = 2^(x*log2(e))/2 | st(0) = 2^(-x*log2(e))-1
-  fld1					;st(2) = 2^(x*log2(e))/2 | st(1) = 2^(-x*log2(e))-1 | st(0) = 1
-  faddp					;st(1) = 2^(x*log2(e))/2 | st(0) = 2^(-x*log2(e))
-  fld1					;st(2) = 2^(x*log2(e))/2 | st(1) = 2^(-x*log2(e)) | st(0) = 1
-  fld1					;st(3) = 2^(x*log2(e))/2 | st(2) = 2^(-x*log2(e)) | st(1) = 1 | st(0) = 1
-  faddp					;st(2) = 2^(x*log2(e))/2 | st(1) = 2^(-x*log2(e)) | st(0) = 2
-  fdivp					;st(1) = 2^(x*log2(e))/2 | st(0) = 2^(-x*log2(e))/2
-  fsubrp				;st(0) = 2^(x*log2(e))/2 - 2^(-x*log2(e))/2
+  fmulp					;st(1) = 2^(x*log2(e))/2 | st(0) = -x*log2(e)
+  fld st0				;st(2) = 2^(x*log2(e))/2 | st(1) = -x*log2(e) | st(0) = -x*log2(e)
+  frndint				;st(2) = 2^(x*log2(e))/2 | st(1) = -x*log2(e) | st(0) = INT[-x*log2(e)]
+  fsub st1,st0			;st(2) = 2^(x*log2(e))/2 | st(1) = -x*log2(e)-INT[-x*log2(e)] | st(0) = INT[-x*log2(e)]
+  fxch st1				;st(2) = 2^(x*log2(e))/2 | st(1) = INT[-x*log2(e)] | st(0) = -x*log2(e)-INT[-x*log2(e)]
+  f2xm1					;st(2) = 2^(x*log2(e))/2 | st(1) = INT[-x*log2(e)] | st(0) = 2^(-x*log2(e)-INT[-x*log2(e)])-1
+  fld1					;st(3) = 2^(x*log2(e))/2 | st(2) = INT[-x*log2(e)] | st(1) = 2^(-x*log2(e)-INT[-x*log2(e)])-1 | st(0) = 1
+  faddp					;st(2) = 2^(x*log2(e))/2 | st(1) = INT[-x*log2(e)] | st(0) = 2^(-x*log2(e)-INT[-x*log2(e)])
+  fscale				;st(2) = 2^(x*log2(e))/2 | st(1) = INT[-x*log2(e)] | st(0) = 2^(-x*log2(e))
+  fld1					;st(3) = 2^(x*log2(e))/2 | st(2) = INT[-x*log2(e)] | st(1) = 2^(-x*log2(e)) | st(0) = 1
+  fld1					;st(4) = 2^(x*log2(e))/2 | st(3) = INT[-x*log2(e)] | st(2) = 2^(-x*log2(e)) | st(1) = 1 | st(0) = 1
+  faddp					;st(3) = 2^(x*log2(e))/2 | st(2) = INT[-x*log2(e)] | st(1) = 2^(-x*log2(e)) | st(0) = 2
+  fdivp					;st(2) = 2^(x*log2(e))/2 | st(1) = INT[-x*log2(e)] | st(0) = 2^(-x*log2(e))/2
+  fsubr	st2,st0			;st(2) = 2^(-x*log2(e))/2 - 2^(x*log2(e))/2 | st(1) = INT[-x*log2(e)] | st(0) = 2^(-x*log2(e))/2
+  fxch st2				;st(2) = 2^(-x*log2(e))/2 | st(1) = INT[-x*log2(e)] | st(0) = 2^(-x*log2(e))/2 - 2^(x*log2(e))/2
+  fchs					;st(0) = 2^(x*log2(e))/2 - 2^(-x*log2(e))/2
   
 printresult:
-  fstp qword [inp]
+  fst qword [inp]
   push dword [inp+4]
   push dword [inp]
   push print
