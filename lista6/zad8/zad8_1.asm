85e3a16e1b405bdfc06f74ca039ddf696f0c9c90
Józef Piechaczek <44442022+dzazef@users.noreply.github.com>
2019-01-06 19:11:18 +0100

    Update zad8_1.asm

--- lista6/zad8/zad8_1.asm
+++ lista6/zad8/zad8_1.asm
@@ -16,7 +16,7 @@ main:
   call scanf
   
   finit					;moduł zmiennoprzecinkowy
-  fld qword [inp]		;st(0) = x
+  fld qword [inp]		  ;st(0) = x
   fldl2e				;st(1) = x | st(0) = log2(e)
   fyl2x					;st(0) = x*log2(e)
   f2xm1					;st(0) = 2^(x*log2(e))-1
@@ -26,7 +26,7 @@ main:
   fld1					;st(2) = 2^(x*log2(e)) | st(1) = 1 | st(0) = 1
   faddp					;st(1) = 2^(x*log2(e)) | st(0) = 2
   fdivp					;st(0) = 2^(x*log2(e))/2
-  fld qword [inp]		;st(1) = 2^(x*log2(e))/2 | st(0) = x
+  fld qword [inp]		  ;st(1) = 2^(x*log2(e))/2 | st(0) = x
   fchs					;st(1) = 2^(x*log2(e))/2 | st(0) = -x
   fldl2e				;st(2) = 2^(x*log2(e))/2 | st(1) = -x | st(0) = log2(e)
   fyl2x					;st(1) = 2^(x*log2(e))/2 | st(0) = -x*log2(e)
@@ -37,7 +37,7 @@ main:
   fld1					;st(3) = 2^(x*log2(e))/2 | st(2) = 2^(-x*log2(e)) | st(1) = 1 | st(0) = 1
   faddp					;st(2) = 2^(x*log2(e))/2 | st(1) = 2^(-x*log2(e)) | st(0) = 2
   fdivp					;st(1) = 2^(x*log2(e))/2 | st(0) = 2^(-x*log2(e))/2
-  fsubrp				;st(0) = 2^(x*log2(e))/2 - st(0) = 2^(-x*log2(e))/2
+  fsubrp				;st(0) = 2^(x*log2(e))/2 - 2^(-x*log2(e))/2
   
 printresult:
   fstp qword [inp]
